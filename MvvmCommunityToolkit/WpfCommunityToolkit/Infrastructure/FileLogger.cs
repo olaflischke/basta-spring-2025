570d135f9de790f671b058a0797fcad627efbff1
Olaf Lischke <olaf.lischke@lischke-edv.de>
2025-02-28 15:18:54 +0100

    MvvmCommunityToolkit Sample

--- MvvmCommunityToolkit/WpfCommunityToolkit/Infrastructure/FileLogger.cs
+++ MvvmCommunityToolkit/WpfCommunityToolkit/Infrastructure/FileLogger.cs
@@ -1,23 +1,32 @@
 using Microsoft.Extensions.Logging;
-using System;
 using System.IO;
 
 namespace WpfCommunityToolkit.Infrastructure;
 
+/// <summary>
+/// Implementierung von <see cref="ILogger"/> für einen einfachen FileLogger.
+/// </summary>
 public class FileLogger : ILogger
 {
     private readonly string _filePath;
     private readonly object _lock = new object();
 
+    /// <summary>
+    /// Konstruktor für einen <see cref="FileLogger"/> mit dem gg. Dateipfad.
+    /// </summary>
+    /// <param name="filePath">Pfad der LogDatei.</param>
     public FileLogger(string filePath)
     {
         _filePath = filePath;
     }
 
+    /// <inheritdoc />
     public IDisposable BeginScope<TState>(TState state) => null!;
 
+    /// <inheritdoc />
     public bool IsEnabled(LogLevel logLevel) => true;
 
+    /// <inheritdoc />
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
     {
         if (!IsEnabled(logLevel))
--- MvvmCommunityToolkit/WpfCommunityToolkit/Infrastructure/FileLoggerProvider.cs
+++ MvvmCommunityToolkit/WpfCommunityToolkit/Infrastructure/FileLoggerProvider.cs
@@ -3,21 +3,39 @@ using System.Collections.Concurrent;
 
 namespace WpfCommunityToolkit.Infrastructure;
 
+/// <summary>
+/// Provider für den FileLogger.
+/// </summary>
 public class FileLoggerProvider : ILoggerProvider
 {
     private readonly string _filePath;
     private readonly ConcurrentDictionary<string, FileLogger> _loggers = new ConcurrentDictionary<string, FileLogger>();
 
+    /// <summary>
+    /// FileLoggerProvider Konstruktor mit dem gg. Dateipfad.
+    /// </summary>
+    /// <param name="filePath">Pfad der LogDatei.</param>
     public FileLoggerProvider(string filePath)
     {
         _filePath = filePath;
     }
 
+    /// <summary>
+    /// Erzeugt eine <see cref="ILogger"/>-Instanz mit dem gg. Kategorienamen.
+    /// </summary>
+    /// <param name="categoryName">Kategoriename</param>
+    /// <returns>Ein <see cref="ILogger"/></returns>
     public ILogger CreateLogger(string categoryName)
     {
         return _loggers.GetOrAdd(categoryName, name => new FileLogger(_filePath));
     }
 
+    /// <summary>
+    /// Dispose-Methode für den Provider.
+    /// </summary>
+    /// <remarks>
+    /// Löscht alle Logger des Providers
+    /// </remarks>
     public void Dispose()
     {
         _loggers.Clear();
